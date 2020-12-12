17ce7f8aff2c3c2ccb3c8391532bf6cce10b7941
Guido Percú <guidopercu@gmail.com>
2020-12-12 17:12:12 -0300

    Basic Selic component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Selic />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Selic from './components/Selic.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Selic
   }
 }
 </script>
---
+++ src/components/Selic.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+  <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+    <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+    <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+      <div class="max-w-md mx-auto">
+        <div>
+          <!--<img src="/img/logo.svg" class="h-7 sm:h-8" /> -->
+          <h1>Taxa SELIC</h1>
+        </div>
+        <div class="divide-y divide-gray-200">
+          <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+            <!--<p>
+               É o principal instrumento de política monetária utilizado pelo Banco Central (BC) para controlar a inflação. Ela influencia todas as taxas de juros do país, como as taxas de juros dos empréstimos, dos financiamentos e das aplicações financeiras
+            </p>-->
+            <ul class="list-disc space-y-2">
+              <li class="flex items-start">
+                <span class="h-6 flex items-center sm:h-7">
+                  <svg class="flex-shrink-0 h-5 w-5 text-cyan-500" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                  </svg>
+                </span>
+                <p class="ml-2">
+                  {{ valor }}
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Selic',
+  props: {
+  },
+  data () {
+    return {
+      valor: null,
+    }
+  },  
+  mounted: function() {
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const api = 'https://9b23t96heh.execute-api.us-east-1.amazonaws.com/dados/selic';
+    fetch(proxyurl + api)
+    .then(response => response.json())
+    .then(content => {
+      this.valor = content.valor
+    })
+    .catch(function(err) {
+      console.log('Fetch Error :-S', err);
+    });
+  }
+}
+</script>
